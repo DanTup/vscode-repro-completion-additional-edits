9fb21514e5e2d58fc568d56326ed81cde7efd0c8
Danny Tuppeny <danny@tuppeny.com>
2019-12-19 10:51:30 +0000

    Add repro

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,29 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+	const pos = new vscode.Position(0, 0);
+	const range = new vscode.Range(pos, pos);
+
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: "markdown" }, {
+		provideCompletionItems: (document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) => {
+			// Build a string like `test(onPressed: [null])` where [null] is
+			// a placeholder containing "null".
+
+			const item = new vscode.CompletionItem("dantup");
+			item.insertText = "dantup";
+			item.additionalTextEdits = [
+				new vscode.TextEdit(range, "additional-dantup"),
+			];
+			return [item];
+		}
+	}));
+
+	const doc = await vscode.workspace.openTextDocument({ language: "markdown" });
+	const results = await (vscode.commands.executeCommand("vscode.executeCompletionItemProvider", doc.uri, pos, undefined, 100000) as Thenable<vscode.CompletionList>);
+	const result = results.items.find((c) => c.label === "dantup")!;
+	console.warn(result.label);
+	console.warn(result.additionalTextEdits);
 }
 
 // this method is called when your extension is deactivated
